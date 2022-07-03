4d7c40b62ef2daccc3d046e4c18e8238de541f35
Daniel Hauk <daniel.hauk@codecamp-n.com>
2022-07-03 13:34:04 +0200

    add dynamic post loading

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -1,8 +1,21 @@
 /** @jsx h */
-import { h } from "preact";
+import { h, render } from "preact";
 import { tw } from "@twind";
+import { Handlers, PageProps } from '$fresh/server.ts'
+import { loadPost, Post } from "../../utils/posts.ts";
 
-export default function Home() {
+export const handler: Handlers<Post> = {
+  async GET(req, ctx) {
+    const id = ctx.params.id;
+    const post = await loadPost(id);
+    if (!post) {
+      return new Response('Post not found', { status: 404 });
+    }
+    return ctx.render(post);
+  }
+}
+
+export default function Home(props: PageProps<Post>) {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       Hello World!
